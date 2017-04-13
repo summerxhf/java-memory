c42a1751ca6b953245ee025052413c6d6efb082e
xinghaifang <xinghaifang@dangdang.com>
2017-04-13 11:53:30 +0800

    add finalize && GC

---
+++ src/GC_Test/FinalizeEscapeGC.java
@@ -0,0 +1,48 @@
+package GC_Test;
+
+/**
+ * Created by xhf on 2017/4/13.
+ * finalize escape GC
+ */
+public class FinalizeEscapeGC {
+    public  static FinalizeEscapeGC SAVE_HOOK = null;
+
+    public void isAlive(){
+        System.out.println("yes,i am still alive:");
+    }
+    @Override
+    protected void finalize() throws Throwable{
+        super.finalize();
+        System.out.println("finalize method executed!");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws Throwable{
+        SAVE_HOOK = new FinalizeEscapeGC();
+
+        //对象第一次成功拯救自己.
+        SAVE_HOOK = null;
+        System.gc();
+        //因为finalize方法优先级很低,所以暂停0.5秒等待.
+        Thread.sleep(500);
+        if(SAVE_HOOK!=null){
+            SAVE_HOOK.isAlive();
+        }else {
+            System.out.println("one: no,i am dead:(");
+        }
+
+
+        //第二次自救失败.finalize只能自救一次.
+        SAVE_HOOK = null;
+        System.gc();
+        //因为finalize方法优先级很低,所以暂停0.5秒等待.
+        Thread.sleep(500);
+        if(SAVE_HOOK!=null){
+            SAVE_HOOK.isAlive();
+        }else {
+            System.out.println("tow : no,i am dead:(");
+        }
+
+
+    }
+}
